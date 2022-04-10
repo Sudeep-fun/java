862e87b408b2cae9a53b375ff10404d2c76fbd2d
Sudeep Maharana <sudeep.maharana@outlook.com>
2022-04-10 10:41:17 +0530

    First committ to the java repo

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+        //Simple declaration of the Hello World using system
+        System.out.println("Hello World!");
+
+    }
+}
